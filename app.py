1a4ad534b45ceb702c32947b70c3f81b9b9d3633
DiceTech453 <amevyematthew@gmail.com>
2025-10-03 00:37:41 +0100

    discord  lib installed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # EXTENSIONS
     # python create Environment Ctr + Shift + P
     # Type: python create Environment
+    # REPL-Read, Evaluate, Print, Loop
 # python by Microsoft
 # python debugger by Microsoft
 # pylance by Microsoft
