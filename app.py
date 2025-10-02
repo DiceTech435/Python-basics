8941d0011f81a266f3f7e8c6f6a217ae43925ccd
DiceTech453 <amevyematthew@gmail.com>
2025-10-03 00:25:42 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,86 @@
+# EXTENSIONS
+    # python create Environment Ctr + Shift + P
+    # Type: python create Environment
+# python by Microsoft
+# python debugger by Microsoft
+# pylance by Microsoft
+
+
+# DATA TYPES
+# str
+# numbers
+    # int -- 40, 3, 90
+    # float -- 4.2, 49.1
+# bool
+
+# BUILT-IN FUNCTIONS
+# For tasks
+# print()
+# input() #always otten as a string, hence convert to int or float
+# For data type format
+# int()
+# float()
+# bool()
+# type()
+# For string
+# len() #gets the length of a string
+# Maths/numbers
+# round()
+# abs()
+# import math (math module)
+# math.ceil(2.9)
+# math.floor(2.9)
+
+
+# METHODS
+# For string
+# .upper()
+# .lower()
+# .title()
+# .find('p')
+# .replace('Boy', 'Girl')
+
+# BOOLEAN EXP/OPERATOR
+# in
+# course = 'Python for Beginners'
+# print('python' in course)
+
+
+# CONCATENATION
+# name = input("What is your name? ")
+# color = input("What color do you like? ")
+# print("Hi " + name + " you like " + color)
+
+# birth_year = input('Birth year: ')
+# age = 2019 - int(birth_year)
+# print(age) 
+
+# MULTI-LINE COMMENTS
+course = '''
+Hi John, 
+Here is our first email to you.
+
+Thank you, 
+The support team
+'''
+
+# CHAR INDEX []
+course = 'Python for beginners'
+print(course[-2])
+print(course[0:3])
+
+# FORMATTED STRING/placeholders
+# first = "John"
+# last = "Smith"
+# message = first '[' + last + '] is a coder'
+# msg = f'{first} [{last}] is a coder'
+
+# course = 'Python for Beginners'
+# print(len(course))
+
+# OPERATORS
+# Arithmetic +, - *, /, //, %, ^, **,
+# Argumented assignment +=, -=, *=, 
+# Opertaor precedence BODMAS
+# Logical operators and, or, not
+# Comparison operators <, >, <=, >=, ==, ===, !=, !==
