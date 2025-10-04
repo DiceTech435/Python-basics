70916e4046e31288d07adf73509c2e121b943298
DiceTech453 <amevyematthew@gmail.com>
2025-10-04 03:02:28 +0100

    modules

---
+++ modules.py
@@ -0,0 +1,10 @@
+# MODULES --files with python codes with diifferent sections
+import funcUtils
+from funcUtils import emoji_converter
+from funcUtils import square
+from lists import min
+
+funcUtils.greet_user("James", "Vivian", 26)
+funcUtils.find_min([3, 6, 2, 8, 4, 10])
+print(square(21))
+print(min)
