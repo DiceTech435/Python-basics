d10f6be7e30fd3560c009e9734dc6ce23de130fa
DiceTech453 <amevyematthew@gmail.com>
2025-10-08 03:23:01 +0100

    files basics

---
+++ files.py
@@ -0,0 +1,11 @@
+# WRITE FILES
+# file_write = open('shop.txt', 'w')
+# file_write.write("We are writing a file in python\n")
+# file_write.write("We have writen a file already\n")
+# file_write.close()
+
+# READ FILES
+# file_read = open('shop.txt', 'r')
+# read = file_read.read()
+# print(read)
+# file_read.close()
