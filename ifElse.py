dfdd100c40859523336c51e9d0abb31fc94896f8
DiceTech453 <amevyematthew@gmail.com>
2025-10-09 22:26:30 +0100

    Match Case Statements and iterables

--- ifElse.py
+++ ifElse.py
@@ -65,3 +65,59 @@ print(min_num)
 print(status)
 print(status)
 print(access_level)
+
+
+
+# MATCH-CASE STATEMENT (switch) -- An alternative of using many 'elif' statements. For a cleaner and more readable syntax
+
+def day_of_week(day):
+    match day:
+        case 1:
+            return "It is Sunday"
+        case 2:
+            return "It is Monday"
+        case 3:
+            return "It is Tuesday"
+        case 4:
+            return "It is Wednesday"
+        case 5:
+            return "It is Thursday"
+        case 6:
+            return "It is Friday"
+        case 7:
+            return "It is Saturday"
+        case _: #Wild card
+            return "Not a valid day"
+
+print(day_of_week(1))
+
+
+# def is_weekend(day):
+#     match day:
+#         case "Sunday":
+#             return True
+#         case "Monday":
+#             return False
+#         case "Tuesday":
+#             return False
+#         case "Wednesday":
+#             return False
+#         case "Thursday":
+#             return False
+#         case "Friday":
+#             return False
+#         case "Saturday":
+#             return True
+#         case _: #Wild card
+#             return False
+
+def is_weekend(day):
+    match day:
+        case "Sunday" | "Saturday":
+            return True
+        case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
+            return False
+        case _: #Wild card
+            return False
+
+print(is_weekend("Saturday"))
--- iterable.py
+++ iterable.py
@@ -32,3 +32,48 @@ for value in my_dictionary.values():
 # For both key and value
 for key, value in my_dictionary.items():
     print(f"{key} = {value}")
+
+
+
+
+
+
+# <=================LIST COMPREHENSION=================>
+# A concise way to create lists. Compact and easier to read than traditional loops. 
+# [expression for value in iterable if condition]
+
+# For Numbers Without conditions
+# doubles = []
+# for x in range(1, 11):
+#     doubles.append(x * 2)
+
+# print(doubles)
+
+doubles = [x * 2 for x in range(1, 11)]
+triples = [y * 3 for y in range(1, 11)]
+squares = [z * z for z in range(1, 11)]
+print(doubles)
+print(triples)
+print(squares)
+
+# For Numbers With conditions
+numbers = [1, -2, 3, -4, 5, -6, -7]
+positive_nums = [num for num in numbers if num >= 0]
+negative_nums = [num for num in numbers if num < 0]
+even_nums = [num for num in numbers if num % 2 == 0]
+odd_nums = [num for num in numbers if num % 2 == 1]
+print(positive_nums)
+print(positive_nums)
+print(even_nums)
+print(odd_nums)
+
+grades = [85, 42, 79, 90, 56, 61, 30]
+passing_grades = [grade for grade in grades if grade >= 60]
+print(passing_grades)
+
+# For strings without conditions
+fruits = ["apple", "orange", "banana", "cocunut"]
+fruits = [fruit.upper() for fruit in fruits]
+fruits_chars = [fruit[0] for fruit in fruits]
+print(fruits)
+print(fruits_chars)
